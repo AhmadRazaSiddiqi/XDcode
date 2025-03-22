511fb476a6985d5f6c64c7c4affc05bb1776707c
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-22 20:53:51 +0500

    Rebuild Backend

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ import cors from 'cors'
 import { dbConnection } from "./utils/dbConnection.js"
 import { route } from "./routes/routes.js"
 import cookieParser from "cookie-parser"
-import path from 'path'
 const PORT = process.env.PORT || 8000
-const __dirname=path.resolve()
 const app = express()
 
 dbConnection()
@@ -21,13 +19,6 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", route)
 app.use("/api/file", route)
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client/dist")))
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
-  })
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`)
