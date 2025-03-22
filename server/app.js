5d0929831029558d9f1218582233fa1bdd59cae1
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-22 21:59:11 +0500

    Updated Cors

--- server/app.js
+++ server/app.js
@@ -9,9 +9,7 @@ const app = express()
 
 dbConnection()
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL 
-    : "http://localhost:5173",
+  origin:process.env.FRONTEND_URL, 
   credentials: true, 
 }))
 app.use(express.json())
