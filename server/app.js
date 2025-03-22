455622c688715791b3012cded13e8ce2c2f4ce45
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-22 23:29:25 +0500

    added env for origin

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 dbConnection()
-app.use(cors({  origin: "http://localhost:5173",
+app.use(cors({  origin: process.env.FRONTEND_URL,
   credentials: true, 
 }))
 app.use(express.json())
