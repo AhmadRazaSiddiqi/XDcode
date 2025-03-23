2230192e59a828e70bfa9b020b0d5fee8bf3da54
AhmadRazaSiddiqi <ahmadrazasiddiqi5@gmail.com>
2025-03-23 13:08:49 +0500

    Removed Unnecessary logs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,6 @@
-import React, { useEffect } from 'react';
-import { useLoggedIn } from './Hooks/LoggedinContext';
-import Cookies from 'js-cookie';
 import Home from '../pages/Home.jsx'
 
 const App = () => {
-  const { token } = useLoggedIn();
-
-  useEffect(() => {
-    const cookieToken = Cookies.get('token');
-    console.log('Token from Context:', token);
-    console.log('Token from Cookie:', cookieToken);
-  }, [token]);
-
   return (
     <Home/>
   )
